9e29dea2d4220e4b9c4222369e0fea6f29cd4b36
Shun Ueda <hasundue@gmail.com>
2021-08-27 16:09:36 +0900

    Modify runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using Calmato
 using Test
 
 @testset "Calmato.jl" begin
-    @test_nowarn db = read_tdb(joinpath("test", "tdb", "cuzn_liang.tdb"))
+    @test_nowarn db = read_tdb(joinpath("tdb", "cuzn_liang.tdb"))
     @test_nowarn print(db)
 end
