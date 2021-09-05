12d36269db2690fd0914882cd86d3b09c198b937
Shun Ueda <hasundue@gmail.com>
2021-09-05 23:54:33 +0900

    Simplify runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,7 @@ using Calmato
 using Test
 
 @testset "Calmato.jl" begin
-    @testset "Database" begin
-        db = read_tdb(joinpath("tdb", "cuzn_liang.tdb"))
-        print(db)
-    end
-    @testset "Equilib" begin
-        sys = init_system(db)
-        res = equilib(sys)
-    end
+    db = read_tdb(joinpath("tdb", "cuzn_liang.tdb"))
+    sys = init_system(db)
+    res = equilib(sys)
 end
