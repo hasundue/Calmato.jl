d2ec524734050bf244a983db67a0d93f261a7c1a
Shun Ueda <hasundue@gmail.com>
2021-09-26 10:37:00 +0900

    Remove sublattices which only includes vacancies in select

--- src/database.jl
+++ src/database.jl
@@ -476,24 +476,38 @@ function select(db::Database, elnames::AbstractString)
 end
 
 function select(phas::Phase, elnames::Vector{<:AbstractString})
-    cons = select(phas.cons, elnames)
+    cons, nonempty = select(phas.cons, elnames)
     [] in cons && return nothing
+
+    sites = [ phas.sites[s] for s in nonempty ]
 
     params = Parameter[]
     for param in phas.params
         if iscomposedof(param.comb, vcat(elnames, "Va"))
-            push!(params, param)
+            comb = [ param.comb[s] for s in nonempty ]
+            push!(params, Parameter(param.name, 
+                                    param.symbol,
+                                    comb,
+                                    param.order,
+                                    param.temp,
+                                    param.funcstr))
         end
     end
-    return Phase(phas.name, phas.state, phas.type, phas.sites, cons, params)
+
+    return Phase(phas.name, phas.state, phas.type, sites, cons, params)
 end
 
 function select(cons::Constitution, elnames::Vector{<:AbstractString})
     latts = Sublattice[]
-    for latt in cons
-        push!(latts, filter(spec -> iscomposedof(spec, vcat(elnames, "Va")), latt))
+    nonempty = Int[]
+    S = length(cons)
+    for s in 1:S
+        latt = filter(spec -> iscomposedof(spec, vcat(elnames, "Va")), cons[s])
+        latt == [] || latt == ["Va"] && continue
+        push!(latts, latt)
+        push!(nonempty, s)
     end
-    return latts
+    return latts, nonempty
 end
 
 function composedof(spec::AbstractString)
